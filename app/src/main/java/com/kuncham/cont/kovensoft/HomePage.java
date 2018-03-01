9aac83848c426dbab10b32a4861bce44ea30a91c
admin <cdastagirireddy@hotmail.com>
2018-03-01 16:57:13 +0530

    contact us link

--- app/src/main/java/com/kuncham/cont/kovensoft/HomePage.java
+++ app/src/main/java/com/kuncham/cont/kovensoft/HomePage.java
@@ -132,6 +132,9 @@ public class HomePage extends AppCompatActivity
 
         }else if (id == R.id.nav_contactus) {
 
+            Intent contactIntent = new Intent(this,ContactUs.class);
+            startActivity(contactIntent);
+
         }else if (id == R.id.nav_share) {
 
         }
