b22da770844c966d9efcae9d5efa95ffc2a35a88
swarnalatharouthu <swarnalatharouthu@gmail.com>
2018-03-01 17:52:33 +0530

    services java classes

---
+++ app/src/main/java/com/kuncham/cont/kovensoft/Training.java
@@ -0,0 +1,23 @@
+package com.kuncham.cont.kovensoft;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by KSSSYS011 on 3/1/2018.
+ */
+
+public class Training extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View mTrainingView = inflater.inflate(R.layout.activity_training,null);
+        return mTrainingView;
+    }
+}
