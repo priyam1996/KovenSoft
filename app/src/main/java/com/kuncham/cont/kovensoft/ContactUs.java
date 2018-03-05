50187b322a7670e842be819503af0ac30c998e3b
admin <cdastagirireddy@hotmail.com>
2018-03-05 13:16:14 +0530

    contact us social links

--- app/src/main/java/com/kuncham/cont/kovensoft/ContactUs.java
+++ app/src/main/java/com/kuncham/cont/kovensoft/ContactUs.java
@@ -1,15 +1,19 @@
 package com.kuncham.cont.kovensoft;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class ContactUs extends AppCompatActivity {
 
     Button mSubmitButton;
+    ImageView mFacebookLink,mGoogleLink,mYoutubeLink,mLinkedinLink,mBloggerLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,5 +47,58 @@ public class ContactUs extends AppCompatActivity {
 
             }
         });
+
+        mFacebookLink = (ImageView)findViewById(R.id.facebook);
+        mFacebookLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent facebookIntent = new Intent(Intent.ACTION_VIEW);
+                facebookIntent.setData(Uri.parse("https://www.facebook.com/kovensoft/"));
+
+                startActivity(Intent.createChooser(facebookIntent,getResources().getText(R.string.facebook)));
+            }
+        });
+
+        mYoutubeLink = (ImageView)findViewById(R.id.youtube);
+        mYoutubeLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent youtubeIntent = new Intent(Intent.ACTION_VIEW);
+                youtubeIntent.setData(Uri.parse("https://www.youtube.com/channel/UCN7ht9-3QSRYD7opqzV42Zw"));
+                startActivity(youtubeIntent);
+            }
+        });
+
+        mGoogleLink = (ImageView)findViewById(R.id.google_plus);
+        mGoogleLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent googleIntent = new Intent(Intent.ACTION_VIEW);
+                googleIntent.setData(Uri.parse("https://plus.google.com/u/0/102477801303466990194"));
+                startActivity(googleIntent);
+            }
+        });
+
+        mLinkedinLink = (ImageView)findViewById(R.id.linkedin);
+        mLinkedinLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent linkedinIntent = new Intent(Intent.ACTION_VIEW);
+                linkedinIntent.setData(Uri.parse("https://www.linkedin.com/company/kovensoft/"));
+                startActivity(linkedinIntent);
+            }
+        });
+
+        mBloggerLink = (ImageView)findViewById(R.id.blogger);
+        mBloggerLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent bloggerIntent = new Intent(Intent.ACTION_VIEW);
+                bloggerIntent.setData(Uri.parse("https://kovensoft.blogspot.in/"));
+                startActivity(bloggerIntent);
+            }
+        });
+
     }
 }
