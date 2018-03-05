074b3aa9b919374567e75edde07b1b418a72f2a6
priyam1996 <priyankareddythummala@gmail.com>
2018-03-05 10:44:35 +0530

    services

--- app/src/main/java/com/kuncham/cont/kovensoft/Services.java
+++ app/src/main/java/com/kuncham/cont/kovensoft/Services.java
@@ -1,23 +1,15 @@
 package com.kuncham.cont.kovensoft;
 
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.Bundle;
 import android.support.design.widget.TabLayout;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-
-import android.widget.TextView;
 
 public class Services extends AppCompatActivity {
 
@@ -80,7 +72,7 @@ public class Services extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_exit) {
             return true;
         }
 
